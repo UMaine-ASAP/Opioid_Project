2fb09e0d28cbd41709e4e81ce87968f45d2ce885
Jacob Hall <jacob.hall1@maine.edu>
2018-03-02 17:57:41 -0500

    Started fleshing out intro screen

--- android/Mindfulness/app/src/main/java/com/asap/mindfulness/IntroductionActivity.kt
+++ android/Mindfulness/app/src/main/java/com/asap/mindfulness/IntroductionActivity.kt
@@ -2,13 +2,28 @@ package com.asap.mindfulness
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_introduction.*
 
 class IntroductionActivity : AppCompatActivity() {
+
+    private var page = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction)
 
+        updatePage(0)
+    }
 
+    fun updatePage(pageNum: Int) {
+        page = pageNum
+        title = when (page) {
+            0 -> getString(R.string.intro_page_one_title)
+            else -> "Mindfulness"
+        }
+        intro_text.text = when(page) {
+            0 -> getString(R.string.intro_page_one_text)
+            else -> "Invalid page?"
+        }
     }
 }
