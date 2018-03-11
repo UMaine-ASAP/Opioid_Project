1f91532dbf4d2685d63dcfb575001278271aa14c
Spencer Ward <spencer.ward@maine.edu>
2018-03-11 16:11:25 -0400

    Added settings to change user's name, toggle notifications, and change notification time

---
+++ android/Mindfulness/app/src/main/java/com/asap/mindfulness/Fragments/NameDialogFragment.kt
@@ -0,0 +1,62 @@
+package com.asap.mindfulness.Fragments
+
+import android.app.DialogFragment
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.asap.mindfulness.R
+import kotlinx.android.synthetic.main.activity_user.*
+import kotlinx.android.synthetic.main.fragment_name_dialog.view.*
+
+
+class NameDialogFragment : DialogFragment() {
+    private lateinit var mPrefs: SharedPreferences
+    private lateinit var mListener: OnNameChangeListener
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                     savedInstanceState: Bundle?): View {
+        val v = inflater.inflate(R.layout.fragment_name_dialog, container, false)
+
+        mPrefs = inflater.context.getSharedPreferences(getString(R.string.sp_file_key), Context.MODE_PRIVATE)
+
+        v.user_name_edit.setText(mPrefs.getString(getString(R.string.sp_name), "New Name"))
+
+        v.user_name_set.setOnClickListener {
+            val newName = v.user_name_edit.text.toString()
+            with (mPrefs.edit()) {
+                putString(getString(R.string.sp_name), newName)
+                apply()
+            }
+            mListener.nameChanged(newName)
+            dismiss()
+        }
+
+        return v
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnNameChangeListener) mListener = context
+    }
+
+    companion object {
+
+        /**
+         * Create a new instance of MyDialogFragment, providing "num"
+         * as an argument.
+         */
+        internal fun newInstance(): NameDialogFragment {
+            return NameDialogFragment()
+        }
+    }
+
+    interface OnNameChangeListener {
+        abstract fun nameChanged(newName: String)
+    }
+}
--- android/Mindfulness/app/src/main/java/com/asap/mindfulness/UserActivity.kt
+++ android/Mindfulness/app/src/main/java/com/asap/mindfulness/UserActivity.kt
@@ -1,13 +1,23 @@
 package com.asap.mindfulness
 
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings.Secure
+import com.transitionseverywhere.TransitionManager
 import kotlinx.android.synthetic.main.activity_user.*
+import android.content.pm.PackageManager
+import android.content.ComponentName
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.asap.mindfulness.Fragments.NameDialogFragment
+import com.asap.mindfulness.Notifications.NotificationBootHandler
 
-class UserActivity : AppCompatActivity() {
+
+class UserActivity : AppCompatActivity(), NameDialogFragment.OnNameChangeListener {
 
     lateinit var mPrefs: SharedPreferences
 
@@ -23,6 +33,18 @@ class UserActivity : AppCompatActivity() {
         val userTracks = mPrefs.getInt(getString(R.string.sp_tracks_completed), 0)
 
         user_name.text = userName
+        user_name.setOnClickListener { _ ->
+            val fragTransaction = fragmentManager.beginTransaction()
+            val prevFrag = fragmentManager.findFragmentByTag("user_name_dialog")
+            if (prevFrag != null) {
+                fragTransaction.remove(prevFrag)
+            }
+            fragTransaction.addToBackStack(null)
+
+            NameDialogFragment.newInstance().show(fragTransaction, "user_name_dialog")
+        }
+
+
         user_study_id.text = getString(R.string.user_study_id, userId)
         user_progress_days.text = if (userDays > 1) {
             resources.getQuantityString(
@@ -36,6 +58,68 @@ class UserActivity : AppCompatActivity() {
         } else {
             getString(R.string.user_progress_tracks_none)
         }
+
+        user_notifications_switch.isChecked = mPrefs.getBoolean(getString(R.string.sp_notification_enabled), true)
+        user_notifications_switch.setOnClickListener { _ ->
+            TransitionManager.beginDelayedTransition(user_views)
+            if (user_notifications_switch.isChecked) {
+                // Update the UI
+                user_notifications_enabled.text = getString(R.string.user_notify_enabled)
+                // Update settings
+                with (mPrefs.edit()) {
+                    putBoolean(getString(R.string.sp_notification_enabled), true)
+                    apply()
+                }
+                // Enable the on-boot receiver
+                val receiver = ComponentName(this, NotificationBootHandler::class.java)
+                packageManager.setComponentEnabledSetting(receiver,
+                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP)
+            } else {
+                // Update the UI
+                user_notifications_enabled.text = getString(R.string.user_notify_disabled)
+                // Update settings
+                with (mPrefs.edit()) {
+                    putBoolean(getString(R.string.sp_notification_enabled), false)
+                    apply()
+                }
+                // Disable the on-boot receiver
+                val receiver = ComponentName(this, NotificationBootHandler::class.java)
+                packageManager.setComponentEnabledSetting(receiver,
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP)
+            }
+        }
+
+        val timePickerListener = { _: View, h: Int, m: Int ->
+            with (mPrefs.edit()) {
+                putInt(getString(R.string.sp_notification_hour), h)
+                putInt(getString(R.string.sp_notification_minute), m)
+                apply()
+            }
+
+            val amPM: String
+            val h12 = if (h < 12) {
+                amPM = "am"
+                h
+            } else {
+                amPM = "pm "
+                h - 12
+            }
+
+            user_notifications_time_picker.text = String.format("%d:%02d %s", h12, m, amPM)
+        }
+
+        user_notifications_time_picker.setOnClickListener { _ ->
+            val hour = mPrefs.getInt(getString(R.string.sp_notification_hour), 8)
+            val min = mPrefs.getInt(getString(R.string.sp_notification_minute), 0)
+            val tp = TimePickerDialog(this, R.style.TimePickerDialogStyle, timePickerListener, hour, min, false)
+            tp.show()
+        }
+    }
+
+    override fun nameChanged(newName: String) {
+        user_name.text = newName
     }
 
     companion object {
